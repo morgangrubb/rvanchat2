64d399f00e712792bbb24fc941917209d019b5ad
Miggles <miggles@gmail.com>
2015-08-24 08:09:20 -0700

    Basic bot commands

--- lib/bot.rb
+++ lib/bot.rb
@@ -7,11 +7,14 @@ require 'jabbot'
 require 'uri'
 
 configure do |conf|
-  conf.nick = "bot"
+  conf.nick = "@wopr"
   conf.login = "bot@#{XMPP_HOST}"
   conf.channel = MAIN_ROOM
   conf.server = CONFERENCE_HOST
   conf.password = "thisisthebotpassword"
+  conf.log_level = 'info'
+  conf.log_file = "/home/ubuntu/rvanchat/current/log/bot.log"
+  conf.debug = true
 end
 
 ACTUAL_ROOM = Room.where(name: MAIN_ROOM).first
@@ -30,14 +33,16 @@ message do |message, params|
   user_id = User.where(xmpp_username: message.user).first.try(:id)
   stored_message =
     Message.create(
-      user_name: message.user.encode("UTF-8"),
+      user_name: message.user,
       room_name: ACTUAL_ROOM.name,
       user_id: user_id,
       room_id: ACTUAL_ROOM.id,
-      text: CGI::escape(message.text)
+      text: message.text
     )
 
-  URI.extract(message.text).each do |url|
+  links = URI.extract(message.text)
+
+  links.each do |url|
     uri = URI.parse(url)
     Link.create(
       message_id: stored_message.id,
@@ -45,6 +50,34 @@ message do |message, params|
       url: uri.to_s
     )
   end
+
+  links.each do |url|
+    # Does it look like Twitter, Facebook or Youtube?
+  end
+end
+
+message /^hi,? @?wopr/i do |message, params|
+  post "Hi, #{message.user}"
+  post "Use !help for a list of commands" => message.user
+end
+
+message "!help" do |message, params|
+  response = <<-EOS
+I understand these commands:
+  !help
+
+These commands will eventually work:
+  !giphy
+  !google
+  !youtube
+  !meme
+  !stats
+
+I will also eventually capture and parse Twitter, Facebook and Youtube links, too.
+
+For the time being I'm a bit stupid.
+  EOS
+  post response => message.user
 end
 
 # TODO: Giphy api
