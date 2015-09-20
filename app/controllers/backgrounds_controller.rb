1c36958792936e847fd1ebde3b71209e81e7e8f9
Miggles <miggles@gmail.com>
2015-09-20 15:11:06 -0700

    Default background

--- app/controllers/backgrounds_controller.rb
+++ app/controllers/backgrounds_controller.rb
@@ -1,5 +1,14 @@
 class BackgroundsController < ApplicationController
   def random
-    render json: Background.offset(rand(Background.count)).first
+    count = Background.count
+    if count.zero?
+      render json: {
+        url: view_context.image_path("footer_lodyas.png"),
+        credit: ""
+      }
+    else
+      render json: Background.offset(rand(Background.count)).first
+    end
+
   end
 end
