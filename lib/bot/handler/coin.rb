2d3afb37e5d96e616c5bce1d322f887d16dc25f4
Miggles <miggles@gmail.com>
2015-10-12 12:56:50 -0700

    Fixed coin toss

--- lib/bot/handler/coin.rb
+++ lib/bot/handler/coin.rb
@@ -4,24 +4,42 @@ module Bot
 
       def private_message(message, params)
         case message.text
-        when /^!coin(\s+.*)?$/
+        when /^!(coin|cointoss|coinflip|flipcoin|flip)(\s+.*)?$/
           message.processed!
           say "You get #{flip_coin}.", user_name: message.user_name
+        when /^!(tails|heads)$/
+          message.processed!
+          toss = flip_coin
+          if toss == $1
+            say "#{toss.capitalize}, you win.", user_name: message.user_name
+          else
+            say "#{toss.capitalize}, you lose.", user_name: message.user_name
+          end
         end
       end
 
       def public_message(message, params)
         case message.text
-        when /^!coin(\s+.*)?$/
+        when /^!(coin|cointoss|coinflip|flipcoin|flip)(\s+.*)?$/
           message.processed!
           say "#{message.user_name} gets #{flip_coin}."
+        when /^!(tails|heads)$/
+          message.processed!
+          toss = flip_coin
+          if toss == $1
+            say "#{toss.capitalize}, #{message.user_name} wins."
+          else
+            say "#{toss.capitalize}, #{message.user_name} loses."
+          end
         end
       end
 
       def describe_commands(message)
         [
           "Flip a coin with:",
-          "!coin"
+          "!coin, !cointoss, !coinflip, !flipcoin, !flip",
+          "!heads",
+          "!tails"
         ].join("\n")
       end
 
