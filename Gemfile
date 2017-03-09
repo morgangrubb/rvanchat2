633f4da3ba266c6e58a971e72dd7dfb0076fb2a7
Miggles <miggles@gmail.com>
2017-03-08 19:31:59 -0800

    Lock capistrano at a compatible version

--- Gemfile
+++ Gemfile
@@ -77,7 +77,8 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 
-  gem 'capistrano-rails'
+  gem 'capistrano', '3.4.0'
+  gem 'capistrano-rails', '1.1.3'
   gem 'capistrano-rbenv'
   gem 'capistrano-passenger'
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,22 @@ lock '3.4.0'
 
 set :application, 'rvanchat'
 set :repo_url, 'git@github.com:morgangrubb/rvanchat2.git'
-set :rbenv_ruby, '2.2.1'
+
+# Capfile
+require 'capistrano/rbenv'
+
+# config/deploy.rb
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+# set :rbenv_ruby, '2.0.0-p247'
+
+# in case you want to set ruby version from the file:
+set :rbenv_ruby, File.read('.ruby-version').strip
+
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
+
+# set :rbenv_ruby, '2.2.1'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
